1480a8aec5b2f80f32bd74df5dfae04cb036812d
Nucly <nucly@proton.me>
2025-03-13 15:01:50 +0700

    added path from argument

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from stats import convert_dict, count_words, count_characters
+import sys
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    path = "books/frankenstein.txt"
-    book = get_book_text(path)
+    book = None
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book = get_book_text(sys.argv[1])
+
     characters = count_characters(book)
     characters = convert_dict(characters)
+
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path}")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print(f"Found {count_words(book)} total words")
-    print("--------- Character Count -------")
 
+    print("--------- Character Count -------")
     for ch in characters:
         if ch["char"].isalpha():
             print(f'{ch["char"]}: {ch["num"]}')
